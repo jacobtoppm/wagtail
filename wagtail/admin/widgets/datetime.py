1832d0f4dab89ab03b08df69741c25a100c491da
jacobtoppm <jacobtm@torchbox.com>
2020-05-22 11:18:10 +0100

    Make datetimepicker consistent with USE_L10N=True by allowing it to override the WAGTAIL_x_FORMAT settings. Add a new setting, WAGTAIL_TIME_FORMAT, to specify the time formatting for the timepicker widget and the input format.

--- wagtail/admin/widgets/datetime.py
+++ wagtail/admin/widgets/datetime.py
@@ -10,6 +10,7 @@ from wagtail.admin.staticfiles import versioned_static
 
 
 DEFAULT_DATE_FORMAT = '%Y-%m-%d'
+DEFAULT_TIME_FORMAT = '%H:%M'
 DEFAULT_DATETIME_FORMAT = '%Y-%m-%d %H:%M'
 
 
@@ -22,7 +23,10 @@ class AdminDateInput(widgets.DateInput):
         if attrs:
             default_attrs.update(attrs)
         if fmt is None:
-            fmt = getattr(settings, 'WAGTAIL_DATE_FORMAT', DEFAULT_DATE_FORMAT)
+            if getattr(settings, 'USE_L10N', False):
+                fmt = get_format('DATE_INPUT_FORMATS')[0]
+            else:
+                fmt = getattr(settings, 'WAGTAIL_DATE_FORMAT', DEFAULT_DATE_FORMAT)
         self.js_format = to_datetimepicker_format(fmt)
         super().__init__(attrs=default_attrs, format=fmt)
 
@@ -47,11 +51,29 @@ class AdminDateInput(widgets.DateInput):
 class AdminTimeInput(widgets.TimeInput):
     template_name = 'wagtailadmin/widgets/time_input.html'
 
-    def __init__(self, attrs=None, format='%H:%M'):
+    def __init__(self, attrs=None, format=None):
         default_attrs = {'autocomplete': 'new-time'}
         if attrs:
             default_attrs.update(attrs)
-        super().__init__(attrs=default_attrs, format=format)
+        fmt = format
+        if fmt is None:
+            if getattr(settings, 'USE_L10N', False):
+                fmt = get_format('TIME_INPUT_FORMATS')[0]
+            else:
+                fmt = getattr(settings, 'WAGTAIL_TIME_FORMAT', DEFAULT_DATETIME_FORMAT)
+        self.js_format = to_datetimepicker_format(fmt)
+        super().__init__(attrs=default_attrs, format=fmt)
+
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs)
+
+        config = {
+            'format': self.js_format,
+            'formatTime': self.js_format
+        }
+        context['widget']['config_json'] = json.dumps(config)
+
+        return context
 
     @property
     def media(self):
@@ -63,14 +85,24 @@ class AdminTimeInput(widgets.TimeInput):
 class AdminDateTimeInput(widgets.DateTimeInput):
     template_name = 'wagtailadmin/widgets/datetime_input.html'
 
-    def __init__(self, attrs=None, format=None):
+    def __init__(self, attrs=None, format=None, time_format=None):
         default_attrs = {'autocomplete': 'new-date-time'}
         fmt = format
         if attrs:
             default_attrs.update(attrs)
         if fmt is None:
-            fmt = getattr(settings, 'WAGTAIL_DATETIME_FORMAT', DEFAULT_DATETIME_FORMAT)
+            if getattr(settings, 'USE_L10N', False):
+                fmt = get_format('DATETIME_INPUT_FORMATS')[0]
+            else:
+                fmt = getattr(settings, 'WAGTAIL_DATETIME_FORMAT', DEFAULT_DATETIME_FORMAT)
+        time_fmt = time_format
+        if time_fmt is None:
+            if getattr(settings, 'USE_L10N', False):
+                time_fmt = get_format('TIME_INPUT_FORMATS')[0]
+            else:
+                time_fmt = getattr(settings, 'WAGTAIL_TIME_FORMAT', DEFAULT_TIME_FORMAT)
         self.js_format = to_datetimepicker_format(fmt)
+        self.js_time_format = to_datetimepicker_format(time_fmt)
         super().__init__(attrs=default_attrs, format=fmt)
 
     def get_context(self, name, value, attrs):
@@ -79,6 +111,7 @@ class AdminDateTimeInput(widgets.DateTimeInput):
         config = {
             'dayOfWeekStart': get_format('FIRST_DAY_OF_WEEK'),
             'format': self.js_format,
+            'formatTime': self.js_time_format
         }
         context['widget']['config_json'] = json.dumps(config)
 
